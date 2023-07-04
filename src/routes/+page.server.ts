c4911c262540ad7ff9ae640fb1d70a69bb2ac708
Samuel-Martineau <samumartineau@gmail.com>
2023-07-04 19:10:16 -0400

    test2

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
-import '$lib/server/firebase';
+import { FIREBASE_PRIVATE_KEY } from '$env/static/private';
 
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
+	console.log(FIREBASE_PRIVATE_KEY);
 	return {};
 }) satisfies PageServerLoad;
