c1f26552ac1840ff0948f00bf47d0e4f91893e22
Samuel-Martineau <samumartineau@gmail.com>
2023-07-04 19:14:09 -0400

    test2

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,6 @@ import { FIREBASE_PRIVATE_KEY } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	console.log(FIREBASE_PRIVATE_KEY);
+	console.log(JSON.parse(FIREBASE_PRIVATE_KEY));
 	return {};
 }) satisfies PageServerLoad;
